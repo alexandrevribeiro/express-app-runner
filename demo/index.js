10e0da3dd7feff6ce3c0249230db6a0e6be97655
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-12 01:34:21 -0300

    Fixinf method name

--- demo/index.js
+++ demo/index.js
@@ -5,7 +5,7 @@ var indexFilePath = path.join(__dirname, 'index.html');
 var publicDirPath = path.join(__dirname, 'public');
 
 // Defines a index page to be provided by '/'
-runner.setIndexFile(indexFilePath);
+runner.setIndexPage(indexFilePath);
 
 // Making the content of 'public' folder available for the app
 runner.setStaticDir(publicDirPath);
