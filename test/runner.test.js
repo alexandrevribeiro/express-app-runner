3669fc07a17d5742261af13fef6e149b3c21c4d5
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-16 01:37:11 -0300

    Testing "/anything" page

--- test/runner.test.js
+++ test/runner.test.js
@@ -33,13 +33,32 @@ describe('Demo app', function () {
     });
 });
 
-// Testing the index page (/)
+// Testing the users page (/users)
 describe('Demo app', function () {
     it('should provide a "users.html" at "' + appUrl + '/users"', function (done) {
 
         axios.get(appUrl + '/users')
             .then(function (response) {
                 expect(response.data).to.contains('<h1>Users...</h1>');                
+
+                // When you're doing an async test you need to call "done()" after your async code
+                // has completed, and your test has modified everything it needs to modify, so you 
+                // can check the results correctly.
+                done();
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    });
+});
+
+// Testing the "/anything" page
+describe('Demo app', function () {
+    it('should provide the text "Anything!!!" at "' + appUrl + '/anything"', function (done) {
+
+        axios.get(appUrl + '/anything')
+            .then(function (response) {
+                expect(response.data).to.equal('Anything!!!');                
 
                 // When you're doing an async test you need to call "done()" after your async code
                 // has completed, and your test has modified everything it needs to modify, so you 
